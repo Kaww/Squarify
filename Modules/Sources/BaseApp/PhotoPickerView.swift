12cc8eaef8ce133f9af813612fef120457d7dd69
Kaww <rmluux@gmail.com>
2024-03-26 19:32:20 +0100

    Add nice animation to PhotoPicker button

--- Modules/Sources/BaseApp/PhotoPickerView.swift
+++ Modules/Sources/BaseApp/PhotoPickerView.swift
@@ -6,10 +6,26 @@ public extension Color {
     static let risdBlue = Color(uiColor: UIColor(red: 2/255, green: 77/255, blue: 255/255, alpha: 1))
 }
 
+// TODO: Move to utils
+struct IsPressedButtonStyle: ButtonStyle {
+
+    @Binding var isPressed: Bool
+
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .onChange(of: configuration.isPressed) { oldValue, newValue in
+                isPressed = newValue
+            }
+    }
+}
+
 public struct PhotoPickerView: View {
 
     @State private var pickedPhotos: [PhotosPickerItem] = []
+    
     @State private var isLoading = false
+    @State private var isPressingButton = false
+    @State private var hasAppeared = false
 
     @Binding var selection: PhotoSelection?
 
@@ -22,6 +38,10 @@ public struct PhotoPickerView: View {
         .onChange(of: pickedPhotos) {
             process(pickerItems: $1)
         }
+        .task {
+            try? await Task.sleep(for: .seconds(0.2))
+            hasAppeared = true
+        }
     }
 
     private var titleView: some View {
@@ -31,49 +51,62 @@ public struct PhotoPickerView: View {
     }
 
     private var photosPickerButton: some View {
-        PhotosPicker(
-            selection: $pickedPhotos,
-            maxSelectionCount: 10,
-            selectionBehavior: .ordered,
-            matching: .images,
-            preferredItemEncoding: .current,
-            photoLibrary: .shared()
-        ) {
-            photosPickerLabel
-        }
-        .buttonStyle(.plain)
-        .photosPickerStyle(.presentation)
-        .photosPickerDisabledCapabilities([.stagingArea])
-        .transition(.opacity.combined(with: .scale))
-        .frame(maxHeight: .infinity)
-    }
-
-    private var photosPickerLabel: some View {
         VStack(spacing: 16) {
-            Circle()
-                .fill(Color.risdBlue)
-                .shadow(color: .risdBlue.opacity(0.3), radius: 20, x: 0.0, y: 10)
-                .frame(width: 90, height: 90)
-                .overlay {
-                    if isLoading {
-                        ProgressView()
-                            .progressViewStyle(.circular)
-                            .tint(.white)
-                            .controlSize(.large)
-                            .transition(.opacity.combined(with: .scale))
-                    } else {
-                        Image(systemName: "plus")
-                            .font(.system(size: 60, weight: .bold))
-                            .foregroundStyle(.white)
-                            .transition(.opacity.combined(with: .scale))
-                    }
-                }
+            PhotosPicker(
+                selection: $pickedPhotos,
+                maxSelectionCount: 10,
+                selectionBehavior: .ordered,
+                matching: .images,
+                preferredItemEncoding: .current,
+                photoLibrary: .shared()
+            ) {
+                photosPickerLabel
+            }
+            .buttonStyle(IsPressedButtonStyle(isPressed: $isPressingButton))
+            .photosPickerStyle(.presentation)
+            .photosPickerDisabledCapabilities([.stagingArea])
+            .transition(.opacity.combined(with: .scale))
+            .disabled(isLoading)
 
             Text("Pick photos")
                 .font(.system(size: 20, weight: .medium))
                 .opacity(isLoading ? 0 : 1)
+                .opacity(hasAppeared ? 1 : 0)
+                .offset(y: hasAppeared ? 0 : 20)
+                .animation(.spring(duration: 0.4, bounce: 0.5).delay(0.2), value: hasAppeared)
         }
-        .animation(.spring(.bouncy(duration: 0.4)), value: isLoading)
+        .frame(maxHeight: .infinity)
+    }
+
+    private var photosPickerLabel: some View {
+        Circle()
+            .fill(Color.risdBlue)
+            .brightness(isPressingButton ? -0.1 : 0)
+            .shadow(color: .risdBlue.opacity(0.3), radius: 20, x: 0.0, y: 10)
+            .frame(width: 90, height: 90)
+            .scaleEffect(isPressingButton ? 0.9 : 1)
+            .animation(.spring(duration: 0.2), value: isPressingButton)
+            .scaleEffect(hasAppeared ? 1 : 0)
+            .animation(.spring(duration: 0.4, bounce: 0.5), value: hasAppeared)
+            .overlay {
+                if isLoading {
+                    ProgressView()
+                        .progressViewStyle(.circular)
+                        .tint(.white)
+                        .controlSize(.large)
+                        .transition(.opacity.combined(with: .scale))
+                } else {
+                    Image(systemName: "plus")
+                        .font(.system(size: 60, weight: .bold))
+                        .foregroundStyle(.white)
+                        .scaleEffect(isPressingButton ? 0.90 : 1)
+                        .transition(.opacity.combined(with: .scale))
+                        .animation(.spring(duration: 0.3), value: isPressingButton)
+                        .scaleEffect(hasAppeared ? 1 : 0)
+                        .animation(.spring(duration: 0.6, bounce: 0.5), value: hasAppeared)
+                }
+            }
+            .animation(.spring(.bouncy(duration: 0.4)), value: isLoading)
     }
 
     private func process(pickerItems: [PhotosPickerItem]) {
