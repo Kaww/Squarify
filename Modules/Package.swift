f989878d9e33415d25b7fc9d3b4260301d4a8121
Kaww <rmluux@gmail.com>
2024-12-16 21:52:59 +0100

    update package.swift

--- Modules/Package.swift
+++ Modules/Package.swift
@@ -4,135 +4,131 @@
 import PackageDescription
 
 enum Dependency {
-    static let feedbacksKit: Target.Dependency = .product(name: "FeedbacksKit", package: "FeedbacksKit")
-    static let revenueCat: Target.Dependency = .product(name: "RevenueCat", package: "purchases-ios-spm")
-    static let revenueCatUI: Target.Dependency = .product(name: "RevenueCatUI", package: "purchases-ios-spm")
-    static let confettiSwiftUI: Target.Dependency = .product(name: "ConfettiSwiftUI", package: "ConfettiSwiftUI")
+  static let feedbacksKit: Target.Dependency = .product(name: "FeedbacksKit", package: "FeedbacksKit")
+  static let revenueCat: Target.Dependency = .product(name: "RevenueCat", package: "purchases-ios-spm")
+  static let revenueCatUI: Target.Dependency = .product(name: "RevenueCatUI", package: "purchases-ios-spm")
+  static let confettiSwiftUI: Target.Dependency = .product(name: "ConfettiSwiftUI", package: "ConfettiSwiftUI")
 }
 
 let package = Package(
-    name: "Modules",
-    defaultLocalization: "en",
-    platforms: [.iOS(.v17)],
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(name: "BaseApp", targets: ["BaseApp"]),
-        .library(name: "Design", targets: ["Design"]),
-        .library(name: "Localization", targets: ["Localization"]),
-        .library(name: "PhotoEditor", targets: ["PhotoEditor"]),
-        .library(name: "PhotoPicker", targets: ["PhotoPicker"]),
-        .library(name: "Utils", targets: ["Utils"])
-    ],
-    dependencies: [
-        .package(url: "https://github.com/Kaww/FeedbacksKit", from: "1.0.0"),
-        .package(url: "https://github.com/RevenueCat/purchases-ios-spm", from: "4.43.0"),
-        .package(url: "https://github.com/simibac/ConfettiSwiftUI", from: "1.0.0")
-    ],
-    targets: [
-        // MARK: - Base App
-        .target(
-            name: "BaseApp",
-            resources: [
-                 .process("Resources")
-                 // This will process the /Sources/MyLibrary/Resources directory.
-             ],
-            dependencies: [
-                "Design",
-                "PhotoEditor",
-                "PhotoPicker",
-                "Localization",
-                "Utils",
+  name: "Modules",
+  defaultLocalization: "en",
+  platforms: [.iOS(.v17)],
+  products: [
+    // Products define the executables and libraries a package produces, making them visible to other packages.
+    .library(name: "BaseApp", targets: ["BaseApp"]),
+    .library(name: "Design", targets: ["Design"]),
+    .library(name: "Localization", targets: ["Localization"]),
+    .library(name: "PhotoEditor", targets: ["PhotoEditor"]),
+    .library(name: "PhotoPicker", targets: ["PhotoPicker"]),
+    .library(name: "Utils", targets: ["Utils"])
+  ],
+  dependencies: [
+    .package(url: "https://github.com/Kaww/FeedbacksKit", from: "1.0.0"),
+    .package(url: "https://github.com/RevenueCat/purchases-ios-spm", from: "4.43.0"),
+    .package(url: "https://github.com/simibac/ConfettiSwiftUI", from: "1.0.0")
+  ],
+  targets: [
+    // MARK: - Base App
+    .target(
+      name: "BaseApp",
+      dependencies: [
+        "Design",
+        "PhotoEditor",
+        "PhotoPicker",
+        "Localization",
+        "Utils",
 
-                Dependency.confettiSwiftUI,
-                Dependency.feedbacksKit,
-                Dependency.revenueCat,
-                Dependency.revenueCatUI
-            ]
-        ),
-        .testTarget(
-            name: "BaseAppTests",
-            dependencies: [
-                "BaseApp"
-            ]
-        ),
+        Dependency.confettiSwiftUI,
+        Dependency.feedbacksKit,
+        Dependency.revenueCat,
+        Dependency.revenueCatUI
+      ]
+    ),
+    .testTarget(
+      name: "BaseAppTests",
+      dependencies: [
+        "BaseApp"
+      ]
+    ),
 
-        // MARK: - Design
-        .target(
-            name: "Design",
-            dependencies: [
-                "Localization",
-                "Utils",
+    // MARK: - Design
+    .target(
+      name: "Design",
+      dependencies: [
+        "Localization",
+        "Utils",
 
-                Dependency.feedbacksKit
-            ]
-        ),
-        .testTarget(
-            name: "DesignTests",
-            dependencies: [
-                "Design"
-            ]
-        ),
+        Dependency.feedbacksKit
+      ]
+    ),
+    .testTarget(
+      name: "DesignTests",
+      dependencies: [
+        "Design"
+      ]
+    ),
 
-        // MARK: - Localization
-        .target(
-            name: "Localization",
-            dependencies: []
-        ),
-        .testTarget(
-            name: "LocalizationTests",
-            dependencies: [
-                "Localization"
-            ]
-        ),
+    // MARK: - Localization
+    .target(
+      name: "Localization",
+      dependencies: []
+    ),
+    .testTarget(
+      name: "LocalizationTests",
+      dependencies: [
+        "Localization"
+      ]
+    ),
 
-        // MARK: - Photo Editor
-        .target(
-            name: "PhotoEditor",
-            dependencies: [
-                "Design",
-                "Localization",
-                "Utils",
+    // MARK: - Photo Editor
+    .target(
+      name: "PhotoEditor",
+      dependencies: [
+        "Design",
+        "Localization",
+        "Utils",
 
-                Dependency.confettiSwiftUI,
-                Dependency.revenueCat,
-                Dependency.revenueCatUI
-            ]
-        ),
-        .testTarget(
-            name: "PhotoEditorTests",
-            dependencies: [
-                "PhotoEditor",
-            ]
-        ),
+        Dependency.confettiSwiftUI,
+        Dependency.revenueCat,
+        Dependency.revenueCatUI
+      ]
+    ),
+    .testTarget(
+      name: "PhotoEditorTests",
+      dependencies: [
+        "PhotoEditor",
+      ]
+    ),
 
-        // MARK: - Photo Picker
-        .target(
-            name: "PhotoPicker",
-            dependencies: [
-                "Utils",
-                "Design",
-                "Localization"
-            ]
-        ),
-        .testTarget(
-            name: "PhotoPickerTests",
-            dependencies: [
-                "PhotoPicker"
-            ]
-        ),
+    // MARK: - Photo Picker
+    .target(
+      name: "PhotoPicker",
+      dependencies: [
+        "Utils",
+        "Design",
+        "Localization"
+      ]
+    ),
+    .testTarget(
+      name: "PhotoPickerTests",
+      dependencies: [
+        "PhotoPicker"
+      ]
+    ),
 
-        // MARK: - Utils
-        .target(
-            name: "Utils",
-            dependencies: [
-                Dependency.revenueCat
-            ]
-        ),
-        .testTarget(
-            name: "UtilsTests",
-            dependencies: [
-                "Utils"
-            ]
-        )
-    ]
+    // MARK: - Utils
+    .target(
+      name: "Utils",
+      dependencies: [
+        Dependency.revenueCat
+      ]
+    ),
+    .testTarget(
+      name: "UtilsTests",
+      dependencies: [
+        "Utils"
+      ]
+    )
+  ]
 )
